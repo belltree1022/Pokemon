b54809439dd68071c7dba28370e747646c361c92
belltree1022 <s2310078@jaist.ac.jp>
2023-10-15 20:29:24 +0900

    fight,run 両方選択された状態のまま

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -5,12 +5,72 @@ using UnityEngine.UI;
 public class BattleDialogBox : MonoBehaviour
 {
 
+    [SerializeField] Color highlightColor;
+
     //役割:dialogのＴｅｘｔを入力して、変更する
+    [SerializeField] int letterPerSecond; //1文字あたりの表示速度
     [SerializeField] Text dialogText;
+
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
 
     //Textを変更するための関数
     public void SetDialog(string dialog)
     {
         dialogText.text = dialog;
+    }
+
+    //タイプ形式で文字を表示する
+    public IEnumerator TypeDialog(string dialog)
+    {
+
+        dialogText.text = "";
+        foreach (char letter in dialog)
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / letterPerSecond);
+        }
+    }
+
+    //UIの表示
+    //dialogtextの表示
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    //moveselectorの表示
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    //選択中の色を変える
+    public void UpdateActionSelection(int selectAction)
+    {
+        for (int i = 0; i < actionTexts.Count; i++)
+        {
+            if (selectAction == 1)
+            {
+                actionTexts[i].color = highlightColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;
+            }
+        }
     }
 }
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -7,12 +7,61 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+
+    int currentAction; //0:Fight 1:Run
     private void Start()
+    {
+        StartCoroutine(SetupBattle());
+    }
+    IEnumerator SetupBattle()
     {
         playerUnit.Setup(); //ÉÇÉìÉXÉ^Å[ÇÃê∂ê¨Ç∆ï«âÊ
         enemyUnit.Setup();
         //HUDÇÃï«âÊ
         playerHud.SetData(playerUnit.Pokemon);
         enemyHud.SetData(enemyUnit.Pokemon);
+
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} apeared");
+        yield return new WaitForSeconds(1);
+        dialogBox.EnableActionSelector(true);
+        yield return dialogBox.TypeDialog("chose an action");
+
+    }
+
+    private void Update()
+    {
+        //â∫Çì¸óÕÇ∑ÇÈÇ∆Run,è„Çì¸óÕFightÇ…Ç»ÇÈ
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+            {
+                currentAction++;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+            {
+                currentAction--;
+            }
+           
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+
+       if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+
+
+                dialogBox.EnableDialogText(false);
+                dialogBox.EnableActionSelector(false);
+                dialogBox.EnableMoveSelector(true);
+            }
+        }
     }
 }
